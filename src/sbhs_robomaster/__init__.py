94c4fe4631071e85a5f066282c3d5aee052a1218
AndrewPerson <professor_smart@hotmail.com>
2024-06-19 18:23:59 +1000

    Updated docs

--- src/sbhs_robomaster/__init__.py
+++ src/sbhs_robomaster/__init__.py
@@ -14,29 +14,8 @@ A good tutorial on async code is at [https://superfastpython.com/python-asyncio/
 and the documentation is at [https://docs.python.org/3/library/asyncio.html](https://docs.python.org/3/library/asyncio.html).
 
 ## Usage
-```py
-import asyncio
-from sbhs_robomaster import connect_to_robomaster, DIRECT_CONNECT_IP
-
-async def main():
-    async with await connect_to_robomaster(DIRECT_CONNECT_IP) as robot:
-        await robot.set_speed(0.5, 0.5) # Move forward by 0.5m/s and to the right by 0.5m/s
-
-        await asyncio.sleep(1) # Wait 1 second
-
-        await robot.set_speed(0, 0) # Stop moving
-
-        await robot.move(0, 0, 90) # Rotate clockwise by 90 degrees
-
-        # This delay is important because due to limitations of the communication protocol,
-        # the previous `move` call completes before the robot finishes turning.
-        await asyncio.sleep(5) # Wait 5 seconds.
-
-        # Do other stuff
-```
-
-Look at `.client.RoboMasterClient` for a list of available methods on the robot.
-Also look at How To for more examples.
+Look at How To for examples of common operations.
+Also look at `.client.RoboMasterClient` for a list of available methods on the robot.
 
 .. include:: ../../md_docs/how_to.md
 
